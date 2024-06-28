1a82daa0f37d05ebd8476d018eeac8c46c2e8fe2
James King <jamesking.1711@gmail.com>
2024-06-28 09:24:17 +0000

    Added reduce method tutorial.

--- reduce.js
+++ reduce.js
@@ -6,7 +6,19 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) =>{
+  console.log(
+    "Accummulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+// 0 is the explicit initial value, can be anything ie 10 
+}, 0);
+// Can be simplified as on line 10:
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +40,28 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'James',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
+let  totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 
 // Grouping by a property, and totaling it too
+let experienceByProffession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProffession);
